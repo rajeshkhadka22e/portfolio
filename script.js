5ad0572f2abbb5ff4648788f00fcb190c14d932c
Rajesh Khadka <rajeshkhadka22e@gmail.com>
2024-09-02 20:49:06 +0545

    form integration complete

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById('mobile-menu-button').onclick = function () {
+    var menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+}
+
+// Hide mobile menu after clicking on "Home"
+document.getElementById('mobile-menu').onclick = function () {
+    var menu = document.getElementById('mobile-menu');
+    menu.classList.add('hidden');
+}
+function sendMaill(){
+    let parms ={
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value
+
+    }
+    emailjs.send("Dhanmaya", "Rajeshkhadka",parms).then(function (res) {
+alert("email sent !" + res.status);
+
+})
+
+ }
+
+
